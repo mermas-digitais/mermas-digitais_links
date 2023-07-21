0c3e726ee2f3adb9ed325dd48f394dbbc8158c7f
Yasmin Milhomem <yasminmilhomem1517@gmail.com>
2023-07-21 09:51:29 -0300

    interface improvements

--- src/components/Links/Links.jsx
+++ src/components/Links/Links.jsx
@@ -5,8 +5,8 @@ function Links() {
   return (
     <div className='links'>
       <ul>
-        <li><button className='btn-purple'><a href="/">Mermãs Digitais 2022.1</a></button></li>
-        <li><button className='btn-pink'><a href="https://sites.google.com/view/adalovelaceday-ifma/merm%C3%A3s-digitais">Mermãs Digitais 2022.1</a></button></li>
+        <li><button className='btn-purple'><a href="https://sites.google.com/view/adalovelaceday-ifma/merm%C3%A3s-digitais">Mermãs Digitais 2022</a></button></li>
+        <li><button className='btn-pink'><a href="https://www.linkedin.com/company/mermasdigitais/mycompany/">Linkedin do projeto</a></button></li>
       </ul>
     </div>
   )
