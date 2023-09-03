c9f4fb9463a82c1294b25a7f5c9a4b2f3934155f
Vinícius Schneider <mrschneider.v@gmail.com>
2023-09-03 16:22:02 -0300

    add firebaseconfigs, .env file and libs [firebase, firebase-adin, vite-environmentPluggin]

---
+++ src/db/fetch_data.ts
@@ -0,0 +1,1 @@
+import { db, firestore } from "./firebase_configs";
---
+++ src/db/firebase_configs.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import * as firestore from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_FIREBASE_APP_ID,
+  
+}
+console.log(firebaseConfig);
+// Initialize Firebase
+console.log("Firebase initialized");
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export { firestore };
