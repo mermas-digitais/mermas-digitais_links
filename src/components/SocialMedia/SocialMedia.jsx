4d614cf65d1b70f8ce226e4729433ed1006ec848
Yasmin Milhomem <52132167+yasminmilhomem@users.noreply.github.com>
2023-09-03 14:45:29 -0300

    interface improvements

--- src/components/SocialMedia/SocialMedia.jsx
+++ src/components/SocialMedia/SocialMedia.jsx
@@ -6,7 +6,7 @@ function SocialMedia() {
     <div className='social-media'>
       <ul>
         <li><a href="https://mermasdigitais.netlify.app"><Global size={40}/></a></li>
-        <li><a href="/"><Whatsapp size={40}/></a></li>
+        <li><a href="https://wa.me/559981453598?text=Ol%C3%A1%21+Eu+gostaria+de+falar+com+a+equipe+do+Merm%C3%A3s+Digitais."><Whatsapp size={40}/></a></li>
         <li><a href="https://www.instagram.com/mermasdigitais/"><Instagram size={40}/></a></li>
         <li><a href="https://www.youtube.com/@mermasdigitais8008"><Youtube size={40}/></a></li>
       </ul>
