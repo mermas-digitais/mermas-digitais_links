f385c5a005da2a48f545ecc2f693d6ca3e558cab
Yasmin Milhomem <52132167+yasminmilhomem@users.noreply.github.com>
2023-09-03 14:44:57 -0300

    interface improvements

--- src/components/Links/Links.jsx
+++ src/components/Links/Links.jsx
@@ -5,8 +5,9 @@ function Links() {
   return (
     <div className='links'>
       <ul>
-        <li><button className='btn-purple'><a href="https://sites.google.com/view/adalovelaceday-ifma/merm%C3%A3s-digitais">Mermãs Digitais 2022</a></button></li>
-        <li><button className='btn-pink'><a href="https://www.linkedin.com/company/mermasdigitais/mycompany/">Linkedin do projeto</a></button></li>
+        <li><button className='btn-pink'><a href="https://imperatriz.ifma.edu.br/concursos-e-seletivos/?id=17541">Lista de aprovadas e excedentes</a></button></li>
+        <li><button className='btn-purple'><a href="https://imperatriz.ifma.edu.br/concursos-e-seletivos/?d=KyMzdWRdMEtRIkMmUENcRX5oc0B6RHxGZFdEQUNHVXNTRVBBUkFET1JASUZNQWM1M2NjMDJlMDI0NDRmMTNkNjYwNDMwMjYzNDhlN1t8XTAwMl9Qcm9ncmFtYV9JbnN0aXR1Y2lvbmFsX0lUWl9lZGl0YWxfbl9fMTk5XzIwMjMucGRm">Resultado preliminar</a></button></li>
+        <li><button className='btn-pink'><a href="https://imperatriz.ifma.edu.br/concursos-e-seletivos/?id=17541">Edital - Curso Programador Web</a></button></li>
       </ul>
     </div>
   )
